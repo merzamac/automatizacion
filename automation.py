2216f4771090f83781a8d9dd7e55838e374dbfcc
merzamac <mmerza@uc.edu.ve>
2025-05-01 14:17:37 -0400

    Mejoras

--- automation.py
+++ automation.py
@@ -1,11 +1,12 @@
 from playwright.sync_api import sync_playwright
 import os
 
-NUMBER = "00000000000" #colocar el numero
+NUMBER = "00000000"  # colocar el numero
 USER_DATA_DIR = "./whatsapp_session"
 WHATSAPP = "https://web.whatsapp.com/"
 CHAT_URL = f"https://web.whatsapp.com/send?phone={NUMBER}"
-MESSAGE = "" #el mensje va aqui
+MESSAGE = "Prueba Realizada"  # el mensje va aqui
+
 
 def automated_sending(MESSAGE, NUMBER):
     with sync_playwright() as p:
@@ -14,22 +15,20 @@ def automated_sending(MESSAGE, NUMBER):
             channel="chrome",
             headless=False,
             slow_mo=1000,
-            accept_downloads=True,
         )
         page = browser.new_page()
         page.goto(WHATSAPP)
-        page.wait_for_selector('div[id="side"]', timeout=60000)
+        page.wait_for_selector('div[id="side"]', timeout=30000)
         page.goto(CHAT_URL)
-        page.get_by_role("textbox", name="Type a message").get_by_role("paragraph").click()
-        page.get_by_role("textbox", name="Type a message").fill(MESSAGE)
-        page.get_by_role("button", name="Send").click()
-
-
-        page.wait_for_selector('span[aria-hidden="false"]', timeout=60000)
+        page.locator('//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div[2]/div[1]/p').fill(MESSAGE)
+        page.get_by_role("button").and_(page.get_by_label("Send")).click()
+        page.wait_for_selector('span[aria-label=" Read "]', timeout=30000)
+        #page.wait_for_timeout(9999000)
         browser.close()
+
 
 if __name__ == "__main__":
     if not os.path.exists(USER_DATA_DIR):
-        os.makedirs(USER_DATA_DIR)   
-        
+        os.makedirs(USER_DATA_DIR)
+
     automated_sending(MESSAGE, NUMBER)
